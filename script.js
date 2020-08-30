84262334403be3cccebc205ad665268691a73c84
Michal Kowalczyk <mickowalczyk6@gmail.com>
2020-08-31 00:27:10 +0200

    Added show up effect to home section content

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const navbarItems = document.querySelectorAll('.js-navbar-item');
 const sections = document.querySelectorAll('.section');
 
+const contentHide = (index) => {
+    sections[index].querySelector('.js-section-content').classList.remove('show');
+}
+
+const contentShow = (index) => {
+    sections[index].querySelector('.js-section-content').classList.add('show');
+}
+
+contentShow(0);
+
 navbarItems.forEach((navbarItem, itemIndex) => {
     navbarItem.addEventListener('click', () => {
+        if(itemIndex === 0) contentShow(0);
+        else contentHide(0);
         sections[itemIndex].scrollIntoView({behavior: 'smooth'});
     })
 })
